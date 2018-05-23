0372eb5cdaa07edad03d179436e290f0079c2a33
Natasha Cox <natashac1014@gmail.com>
2018-05-23 14:28:53 -0500

    added exclamation points

--- overly-excited.js
+++ overly-excited.js
@@ -2,10 +2,16 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
 
 function addExcitement (theWordArray) {
     let buildMeUp = ""
+    let buildPunctuation = "!"
 
-    for (let i = 0; i < theWordArray.length; i++) {
-        buildMeUp += sentence [i];
-        console.log(buildMeUp);
-    }
+    for (let i = 1; i <= theWordArray.length; i++) {
+      
+          if (i % 3 ===0){
+            buildMeUp += (sentence[i-1] + buildPunctuation + " ")
+            buildPunctuation += "!"
+        } else {
+            buildMeUp += (sentence[i-1] + " ")
+        } console.log(buildMeUp)
+    } ;
 }
-addExcitement(sentence);
+addExcitement(sentence)
